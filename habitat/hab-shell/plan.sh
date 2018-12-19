2d7970cb735905fba971c53bc65052169f8c5125
Danil Guskov <d.guskov@corp.mail.ru>
2018-12-19 18:46:53 +0300

    anonymous build

--- habitat/hab-shell/plan.sh
+++ habitat/hab-shell/plan.sh
@@ -1,7 +1,28 @@
 . ../../plan.sh
 
 pkg_name=hab-shell
-pkg_version='0.1.0'
+pkg_version='0.1.1'
+
+pkg_build_deps=(
+    core/bash
+    core/gawk
+    core/grep
+    core/git
+    core/gcc
+    core/gcc-libs
+    core/make
+    core/clang
+    core/openssl
+    core/pkg-config
+    core/libsodium
+    core/libarchive
+    guskovd/rust-nightly
+)
+
+pkg_deps=(
+    core/sudo
+    core/hab
+)
 
 pkg_bin_dirs=(bin)
 
