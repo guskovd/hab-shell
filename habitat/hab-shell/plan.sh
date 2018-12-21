af668cf9415b679cdac2120e94d3002bd60e4a67
Danil Guskov <d.guskov@corp.mail.ru>
2018-12-21 20:10:11 +0300

    bump version

--- habitat/hab-shell/plan.sh
+++ habitat/hab-shell/plan.sh
@@ -1,7 +1,7 @@
 . ../../plan.sh
 
 pkg_name=hab-shell
-pkg_version='0.1.4'
+pkg_version=pkg_version=_set_from_cargo_
 
 pkg_build_deps=(
     core/bash
@@ -26,9 +26,17 @@ pkg_deps=(
 
 pkg_bin_dirs=(bin)
 
+update_pkg_version() {
+    pkg_version=$(cargo pkgid | awk -F '#' '{print $2}')
+    pkg_artifact="$HAB_CACHE_ARTIFACT_PATH/${pkg_origin}-${pkg_name}-${pkg_version}-${pkg_release}-${pkg_target}.${_artifact_ext}"
+    pkg_dirname="$pkg_name-$pkg_version"
+    pkg_prefix=$HAB_PKG_PATH/${pkg_origin}/${pkg_name}/${pkg_version}/${pkg_release}
+}
+
 do_build() {
     cp -rf ../../{src,Cargo.toml,Cargo.lock} .
     cargo build --release
+    update_pkg_version
 }
 
 do_install() {
