9962c45327fe4ff3bfa494a76a6dd049dad4d335
Danil Guskov <d.guskov@corp.mail.ru>
2018-12-26 00:03:17 +0300

    windows build

--- src/build.rs
+++ src/build.rs
@@ -41,35 +41,20 @@ fn windows_build_command(cache_path: String, root_dir: std::path::PathBuf, args:
         .unwrap()
 }
 
+fn build_command(cache_path: String, root_dir: std::path::PathBuf, args: Vec<&str>) -> std::process::Child {
+    if cfg!(target_os = "linux") {
+        return unix_build_command(cache_path, root_dir, args);
+    } else {
+        return windows_build_command(cache_path, root_dir, args);
+    }
+}
+
 pub fn build(args: Vec<&str>) {
     println!("Building...");
     super::init::init();
     let root_dir = common::project_root();
     let cache_path = format!("{}/.hab-shell-test/cache/keys", common::get_home().display());
-    let mut build = Command::new("docker")
-        .arg("run")
-        .arg("--rm")
-        .arg("--privileged")
-        .arg("-ti")
-        .arg("-e").arg("HAB_NOCOLORING=true")
-        .arg("-e").arg("HAB_ORIGIN=hab-shell")
-        .arg("-v").arg("hab_pkgs:/hab/pkgs")
-        .arg("-v").arg("hab_studios:/hab/studios")
-        .arg("-v").arg(format!("{}:/hab/cache/keys", cache_path))
-        .arg("-v").arg(format!("{}:/hab/cache/artifacts", cache_path))
-        .arg("-v").arg(format!("{}:/src", root_dir.to_str().unwrap()))
-        .arg("-v").arg("/var/run/docker.sock:/var/run/docker.sock")
-        .arg("-w").arg("/src")
-        .arg("dguskov/doha:base")
-        .arg("hab")
-        .arg("studio")
-        .arg("build")
-        .args(args)
-        // .env("HAB_ORIGIN", "hab-shell")
-        // .env("HAB_CACHE_KEY_PATH", cache_path)
-        .current_dir(root_dir)
-        .spawn()
-        .unwrap();
+    let mut build = build_command(cache_path, root_dir, args);
     build.wait().unwrap();
     common::load_env();
     common::lock().unwrap();
