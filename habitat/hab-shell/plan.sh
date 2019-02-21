e19e99e0b3fbc8a820e729aa02041a9861c78b40
Danil Guskov <d.guskov@corp.mail.ru>
2019-02-21 15:43:52 +0300

    static build

--- habitat/hab-shell/plan.sh
+++ habitat/hab-shell/plan.sh
@@ -1,31 +1,58 @@
-. ../../plan.sh
-
+pkg_origin=guskovd
 pkg_name=hab-shell
 pkg_version=pkg_version=_set_from_cargo_
+pkg_description="Habitat Nix shell implementation"
+pkg_maintainer='Guskovd'
+pkg_upstream_url="https://github.com/guskovd/hab-shell"
 
 pkg_build_deps=(
     core/bash
     core/gawk
     core/grep
     core/git
+    core/musl
+    core/zlib-musl
+    core/xz-musl
+    core/bzip2-musl
+    core/libarchive-musl
+    core/openssl-musl
+    core/libsodium-musl
+    core/coreutils
+    core/rust
     core/gcc
-    core/make
-    core/clang
-    core/openssl
-    core/pkg-config
-    guskovd/rust-nightly
 )
 
 pkg_deps=(
     core/sudo
     core/hab
-    core/libsodium
-    core/libarchive
-    core/gcc-libs
     core/docker
 )
 
 pkg_bin_dirs=(bin)
+
+do_prepare() {
+    build_type="--release"
+    export rustc_target="x86_64-unknown-linux-musl"
+    build_line "Setting rustc_target=$rustc_target"
+
+    la_ldflags="-L$(pkg_path_for zlib-musl)/lib -lz"
+    la_ldflags="$la_ldflags -L$(pkg_path_for xz-musl)/lib -llzma"
+    la_ldflags="$la_ldflags -L$(pkg_path_for bzip2-musl)/lib -lbz2"
+    la_ldflags="$la_ldflags -L$(pkg_path_for openssl-musl)/lib -lssl -lcrypto"
+    
+    export LIBARCHIVE_LIB_DIR=$(pkg_path_for libarchive-musl)/lib
+    export LIBARCHIVE_INCLUDE_DIR=$(pkg_path_for libarchive-musl)/include
+    export LIBARCHIVE_LDFLAGS="$la_ldflags"
+    export LIBARCHIVE_STATIC=true
+    export OPENSSL_LIB_DIR=$(pkg_path_for openssl-musl)/lib
+    export OPENSSL_INCLUDE_DIR=$(pkg_path_for openssl-musl)/include
+    export OPENSSL_STATIC=true
+    export SODIUM_LIB_DIR=$(pkg_path_for libsodium-musl)/lib
+    export SODIUM_STATIC=true
+
+    export LD_LIBRARY_PATH=$(pkg_path_for gcc)/lib
+    build_line "Setting LD_LIBRARY_PATH=$LD_LIBRARY_PATH"
+}
 
 update_pkg_version() {
     pkg_version=$(cargo pkgid | awk -F '#' '{print $2}')
@@ -36,15 +63,11 @@ update_pkg_version() {
 
 do_build() {
     cp -rf ../../{src,Cargo.toml,Cargo.lock} .
-    cargo build --release
+    cargo build --target=$rustc_target --release
     update_pkg_version
 }
 
 do_install() {
-    cargo install --root "${pkg_prefix}"
+    install -v -D target/$rustc_target/${build_type#--}/hab-shell "$pkg_prefix"/bin/hab-shell
     ln -sf ${pkg_prefix}/bin/hab-shell ${pkg_prefix}/bin/hs
-}
-
-do_setup_environment() {
-    return 0
 }
